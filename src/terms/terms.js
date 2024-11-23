47ddd718d763f6a94852a73021504417b5da3790
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2024-11-23 13:49:14 +0100

    feat: accept button + its state

--- src/terms/terms.js
+++ src/terms/terms.js
@@ -1,33 +1,37 @@
 import "./terms.css";
 
 let charIndex = 0;
-const message = "Hello World";
+const message = "Hello";
 
 function handleAccept() {}
 
+function resetTextarea(e) {
+  const acceptButton = document.querySelector(".terms-window button");
+  acceptButton.disabled = true;
+  charIndex = 0;
+  e.target.value = "";
+}
+
 function handleInput(e) {
+  const acceptButton = document.querySelector(".terms-window button");
+  const inputChar = e.target.value.charAt(charIndex);
+  const currentMessageChar = message.charAt(charIndex);
   if (e.target.value === "") {
-    charIndex = 0;
-    e.target.value = "";
+    resetTextarea(e);
     return;
   }
   if (e.target.value.length - 1 < charIndex) {
-    charIndex = 0;
-    e.target.value = "";
+    resetTextarea(e);
     return;
   }
-  const inputChar = e.target.value.charAt(charIndex);
-  const currentMessageChar = message.charAt(charIndex);
-  console.log(inputChar, currentMessageChar);
-  if (inputChar === currentMessageChar) {
-    console.log("match");
-  } else {
-    console.log("no match");
-    charIndex = 0;
-    e.target.value = "";
+  if (inputChar !== currentMessageChar) {
+    resetTextarea(e);
     return;
   }
   charIndex++;
+  if (charIndex === message.length) {
+    acceptButton.disabled = false;
+  }
 }
 
 export default function Terms() {
@@ -62,6 +66,7 @@ export default function Terms() {
   termsContent.appendChild(typingTextarea);
   termsContent.appendChild(messageTextarea);
   termsWindow.appendChild(termsContent);
+  termsWindow.appendChild(termsButton);
   TERMS.appendChild(termsWindow);
   MAIN.appendChild(TERMS);
 }
