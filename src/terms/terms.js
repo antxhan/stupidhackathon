c01a1e1ff89878c66d8becc8eb3063a5ede0530e
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2024-11-23 12:58:13 +0100

    feat: basic character checking

--- src/terms/terms.js
+++ src/terms/terms.js
@@ -1,11 +1,55 @@
+import "./terms.css";
+
+let charIndex = 0;
+const message = "Hello World";
+
+function handleInput(e) {
+  if (e.target.value === "") {
+    charIndex = 0;
+    return;
+  }
+  if (e.target.value.length - 1 < charIndex) {
+    charIndex = 0;
+    e.target.value = "";
+    return;
+  }
+  const inputChar = e.target.value.charAt(charIndex);
+  const currentMessageChar = message.charAt(charIndex);
+  console.log(inputChar, currentMessageChar);
+  if (inputChar === currentMessageChar) {
+    console.log("match");
+  } else {
+    console.log("no match");
+  }
+  charIndex++;
+}
+
 export default function Terms() {
   const MAIN = document.querySelector("main");
   const TERMS = document.createElement("div");
   TERMS.classList.add("terms");
-  TERMS.innerHTML = `
-    <h1>Terms of Service</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur rhoncus, sem nunc pretium eros, nec tristique augue purus vel magna. Sed euismod, nisl eget consectetur rhoncus, sem nunc pretium eros, nec tristique augue purus vel magna. Sed euismod, nisl eget consectetur rhoncus, sem nunc pretium eros, nec tristique augue purus vel magna. Sed euismod, nisl eget consectetur rhoncus, sem nunc pretium eros, nec tristique augue purus vel magna. Sed euismod, nisl eget consectetur rhoncus, sem nunc pretium eros, nec tristique augue purus vel magna. Sed euismod, nisl eget consectetur rhoncus, sem nunc pretium eros, nec tristique augue purus vel magna.</p>
 
-    `;
+  const termsWindow = document.createElement("div");
+  termsWindow.classList.add("terms-window");
+
+  const title = document.createElement("h1");
+  title.innerText = "Terms of Service";
+  termsWindow.appendChild(title);
+
+  const termsContent = document.createElement("div");
+  termsContent.classList.add("terms-content");
+
+  const typingTextarea = document.createElement("textarea");
+  typingTextarea.classList.add("terms-content-typing");
+  typingTextarea.addEventListener("input", handleInput);
+
+  const messageTextarea = document.createElement("textarea");
+  messageTextarea.classList.add("terms-content-message");
+  messageTextarea.innerText = message;
+
+  termsContent.appendChild(typingTextarea);
+  termsContent.appendChild(messageTextarea);
+  termsWindow.appendChild(termsContent);
+  TERMS.appendChild(termsWindow);
   MAIN.appendChild(TERMS);
 }
